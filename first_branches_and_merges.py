c0ffe977415ee2196495116c54088babb6097f9c
Gonzalo Gamez <>
2021-09-14 12:39:48 +0300

    meant to say fork no clone

--- first_branches_and_merges.py
+++ first_branches_and_merges.py
@@ -1,7 +1,7 @@
 #This is for group 2 and 3 to learn how to use github source control, branching and merging
 #I have specific challenge for each person
 '''Each person will:
-*clone the repo
+*fork the repo
 *make a branch named after themselves
 *use several commits to show progress on thier branch
 *test that your function works
